27834f4fbab08dae9d19dc587e5c88159f2ca786
Miradouro <rafaelmiradouro@gmail.com>
2021-03-25 16:48:22 -0300

    adicionando inimigos e adicionando final do jogo

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 # Iniciando as fontes
 pygame.font.init()
 main_font = pygame.font.SysFont("comicsans", 50)
+lost_font = pygame.font.SysFont("comicsans", 80)
 
 # Definindo tamanho da tela e colocando
 WIDTH, HEIGHT = 750, 750
@@ -45,6 +46,12 @@ class Ship:
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
 
+    def get_width(self):
+        return self.ship_img.get_width()
+
+    def get_height(self):
+        return self.ship_img.get_height()
+
 
 class Player(Ship):
     def __init__(self, x, y, health=100):
@@ -55,16 +62,39 @@ class Player(Ship):
         self.max_health = health
 
 
+class Enemy(Ship):
+    COLOR_MAP = {
+                "red": (RED_SPACE_SHIP, RED_LASER),
+                "green": (GREEN_SPACE_SHIP, GREEN_LASER),
+                "blue": (BLUE_SPACE_SHIP, BLUE_LASER),
+                }
+
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
+
+
 def main():
     run = True
     FPS = 60
-    level = 1
+    level = 0
     lives = 5
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 5
 
     player_vel = 5
     player = Player(300, 650)
 
     clock = pygame.time.Clock()
+
+    lost = False
+    lost_count = 0
 
     def redraw_window():
         WIN.blit(BG, (0, 0))
@@ -74,13 +104,39 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+        for enemy in enemies:
+            enemy.draw(WIN)
+
         player.draw(WIN)
+
+        if lost:
+            lost_label = lost_font.render("You Lose!!!", 1, (255, 255, 255))
+            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
 
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
         redraw_window()
+
+        if lives <= 0 or player.health <= 0:
+            lost = True
+            lost_count += 1
+
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+            else:
+                continue
+
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH-100),
+                              random.randrange(-1500, -100),
+                              random.choice(["red", "green", "blue"]))
+                enemies.append(enemy)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -89,12 +145,19 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a] and player.x > 0:  # Left
             player.x -= player_vel
-        if keys[pygame.K_d] and player.x < WIDTH - 100:  # Right
+        if keys[pygame.K_d] and player.x < WIDTH - player.get_width():  # Right
             player.x += player_vel
         if keys[pygame.K_w] and player.y > 0:  # Up
             player.y -= player_vel
-        if keys[pygame.K_s] and player.y < HEIGHT - 100:  # Down
+        if keys[pygame.K_s] and player.y < HEIGHT - player.get_height():  # Down
             player.y += player_vel
+
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+
 
 main()
 
