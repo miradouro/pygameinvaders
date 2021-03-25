b5ab8c98e33bdedf4108545a47de7df95e0a5077
Miradouro <rafaelmiradouro@gmail.com>
2021-03-25 11:00:31 -0300

    adicionando background

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import os
 import time
 import random
 
+# Iniciando as fontes
+pygame.font.init()
+main_font = pygame.font.SysFont("comicsans", 50)
+
+# Definindo tamanho da tela e colocando
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("SpaceShip")
+
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
@@ -18,27 +27,37 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # Background
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
-# Definindo tamanho da tela e colocando
-WIDTH, HEIGHT = 750, 750
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("SpaceShip")
 
 # Funções
 
 
 def main():
-    run =True
+    run = True
     FPS = 60
+    level = 1
+    lives = 5
     clock = pygame.time.Clock()
+
+    def redraw_window():
+        WIN.blit(BG, (0, 0))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        pygame.display.update()
 
     while run:
         clock.tick(FPS)
+        redraw_window()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
 
 main()
 
